6757530f9a04dd62f48bf507ad741a2e4cad9226
nirattasai <nirattasai@gmail.com>
2021-10-19 08:46:30 +0700

    fixed date query

--- src/views/user/TaskList.vue
+++ src/views/user/TaskList.vue
@@ -164,7 +164,7 @@ export default {
         resultQuery(){
             if(this.date.month){
                 return this.logList.filter((item)=>{
-                    return this.date.month.toLowerCase().split(' ').every(v => (moment(item.created_at).format('MMMM')).toLowerCase().includes(v))
+                    return this.date.month.toLowerCase().split(' ').every(v => (moment(item.date).format('MMMM')).toLowerCase().includes(v))
                 })
                 }else{
                     return this.logList;
